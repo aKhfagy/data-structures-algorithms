0e8efdf3e843a7cc4194ce06bad49ceb8db23600
aKhfagy <43033631+Ahmed0Khfagy@users.noreply.github.com>
2020-08-17 18:00:04 +0200

    finished assigment 3

--- Courses/Algorithms on Strings/week3_4_kmp_suffix_array_suffix_tree/kmp/kmp.cpp
+++ Courses/Algorithms on Strings/week3_4_kmp_suffix_array_suffix_tree/kmp/kmp.cpp
@@ -7,23 +7,55 @@ using std::cin;
 using std::string;
 using std::vector;
 
+vector<int> computePrefixVector(string& s) {
+    int border = 0, sz = s.size();
+    vector<int> ret(sz, 0);
+
+    for (int i = 1; i < sz; ++i) {
+        while (border > 0 && s[i] != s[border]) {
+            border = ret[border - 1];
+        }
+
+        if (s[i] == s[border]) {
+            ++border;
+            ret[i] = border;
+        }
+
+        if (border == 0) {
+            ret[i] = 0;
+        }
+    }
+
+    return ret;
+}
+
 // Find all occurrences of the pattern in the text and return a
 // vector with all positions in the text (starting from 0) where 
 // the pattern starts in the text.
 vector<int> find_pattern(const string& pattern, const string& text) {
-  vector<int> result;
-  // Implement this function yourself
-  return result;
+    vector<int> result;
+    string s = pattern + '$' + text;
+    int sz = pattern.size(), sz_s = s.size();
+
+    vector<int> prefix = computePrefixVector(s);
+
+    for (int i = sz + 1; i < sz_s; ++i) {
+        if (prefix[i] == sz) {
+            result.emplace_back(i - 2 * sz);
+        }
+    }
+
+    return result;
 }
 
 int main() {
-  string pattern, text;
-  cin >> pattern;
-  cin >> text;
-  vector<int> result = find_pattern(pattern, text);
-  for (int i = 0; i < result.size(); ++i) {
-    printf("%d ", result[i]);
-  }
-  printf("\n");
-  return 0;
+    string pattern, text;
+    cin >> pattern;
+    cin >> text;
+    vector<int> result = find_pattern(pattern, text);
+    for (int i = 0; i < result.size(); ++i) {
+        printf("%d ", result[i]);
+    }
+    printf("\n");
+    return 0;
 }
